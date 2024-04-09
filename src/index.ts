9fd20fd92de282101605517d313c8cce3dce003e
JOHN-AMAO <amaojohn767@gmail.com>
2024-04-09 21:58:01 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import path from "path";
 import simpleGit from "simple-git";
 import { generate } from "./utils";
 import { getAllFiles } from "./file";
+import { uploadFile } from "./aws";
 
 const app = express();
 
@@ -19,8 +20,13 @@ app.post("/deploy", async (req, res) => {
   const id = generate();
   await simpleGit().clone(repoUrl, path.join(__dirname, `./output/${id}`));
   const files = getAllFiles(path.join(__dirname, `./output/${id}`));
-  console.log(files);
-  res.json({ id: id });
+
+  files.forEach(async (file) => {
+    await uploadFile(file.slice(__dirname.length + 1), file);
+  });
+  res.json({
+    id: id,
+  });
 });
 
 app.listen(8000, () => {
